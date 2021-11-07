def954c13da0c6c943068c8defa9c98868eb2374
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-11-06 18:05:52 -0700

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -6,7 +6,7 @@ let x = setInterval(function() {
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("title").innerHTML = "GET HYPED<br>" + days + "d " + hours + "h "
+  document.getElementById("title").innerHTML = "GET FUNKY<br>" + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
   if (distance < 0) {
     clearInterval(x);
